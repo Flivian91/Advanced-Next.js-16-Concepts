d30b1e2b08abc2fe07a0d53ed1601ef6d990716a
Flivian91 <fliviandev@gmail.com>
2025-11-03 22:49:45 +0300

    commit

--- src/schemas/formValidator.js
+++ src/schemas/formValidator.js
@@ -2,5 +2,8 @@
 import { z } from "zod"
 
 const formSchema = z.object({
+  firstName: z.string().min(3).max(50),
+  lastName: z.string().min(3).max(50),
   username: z.string().min(2).max(50),
+  password: z.string().min().max()
 })
